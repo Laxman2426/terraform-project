38a5c51ce5178935f613e1d4a558b15f323d38de
Laxman <125283121+Laxman2426@users.noreply.github.com>
2023-03-21 12:34:27 +0530

    main.tf

--- main.tf
+++ main.tf
@@ -1,14 +1,14 @@
 #this file consists of code for instances and sg
 provider "aws" {
 region = "ap-south-1"
-access_key = "AKIARSPNELGYCJEVYJ4K"
-secret_key = "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"
+access_key = "AKIA5TJKP3VVTGVEYYM7"
+secret_key = "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"
 }
 
 resource "aws_instance" "one" {
   ami             = "ami-0d81306eddc614a45"
   instance_type   = "t2.micro"
-  key_name        = "rmk8s"
+  key_name        = "git1"
   vpc_security_group_ids = [aws_security_group.three.id]
   availability_zone = "ap-south-1a"
   user_data       = <<EOF
@@ -27,7 +27,7 @@ EOF
 resource "aws_instance" "two" {
   ami             = "ami-0d81306eddc614a45"
   instance_type   = "t2.micro"
-  key_name        = "rmk8s"
+  key_name        = "git1"
   vpc_security_group_ids = [aws_security_group.three.id]
   availability_zone = "ap-south-1b"
   user_data       = <<EOF
@@ -68,11 +68,11 @@ resource "aws_security_group" "three" {
 }
 
 resource "aws_s3_bucket" "four" {
-  bucket = "raham0077552bucketterra"
+  bucket = "devopsbucket"
 }
 
 resource "aws_iam_user" "five" {
-name = "rahamuser11" 
+name = "luckyuser " 
 }
 
 resource "aws_ebs_volume" "six" {
